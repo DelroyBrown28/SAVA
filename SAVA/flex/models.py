d95ff269455dacb58c80f8bf497fc937033df6c9
Delroy Brown <delroybrown.db@gmail.com>
2021-03-04 22:52:36 +0000

    Added RichText and SimpleRichText fields

--- SAVA/flex/models.py
+++ SAVA/flex/models.py
@@ -2,17 +2,36 @@
 
 from django.db import models
 from wagtail.core.models import Page
-from wagtail.admin.edit_handlers import FieldPanel
+from streams import blocks
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
+from wagtail.core.fields import StreamField
 
 
 class FlexPage(Page):
+    # @remember:
+    # This page generates the blocks/streamfields on the your flex_page.html.
+    # Copy/Paste this whole class to make other pages flexibe,
+    # Change the template.
     template = 'flex/flex_page.html'
 
-    # content = StreamField()
+    # @remember:
+    # This is the content being rendered on the flex_page.html
+    content = StreamField(
+        [
+            ('title_and_text', blocks.TitleAndTextBlock()),
+            ('full_richtext', blocks.RichTextBlock()),
+            ('simple_richtext', blocks.SimpleRichTextBlock()),
+
+        ],
+        null=True,
+        blank=True
+    )
+
     subtitle = models.CharField(max_length=150, null=True, blank=True)
 
     content_panels = Page.content_panels + [
-        FieldPanel('subtitle')
+        FieldPanel('subtitle'),
+        StreamFieldPanel('content'),
     ]
 
     class Meta:
---
+++ SAVA/streams/blocks.py
@@ -0,0 +1,37 @@
+# @remember: Blocks connect to the streams templates
+from wagtail.core import blocks
+
+
+class TitleAndTextBlock(blocks.StructBlock):
+    title = blocks.CharBlock(required=True, help_text="Add Your Title")
+    text = blocks.TextBlock(required=True, help_text="Add Additional Text")
+
+    class Meta:
+        template = 'streams/title_and_text_block.html'
+        icon = 'edit'
+        label = 'Title & Text'
+
+
+class RichTextBlock(blocks.RichTextBlock):
+
+    class Meta:
+        template = 'streams/richtext_block.html'
+        icon = 'doc-full'
+        label = 'Full Richtext'
+
+
+class SimpleRichTextBlock(blocks.RichTextBlock):
+
+    def __init__(self, required=True, help_text=None, editor='default', features=None, validators=(), **kwargs):
+
+        super().__init__(**kwargs)
+        self.features = [
+            "bold",
+            "italic",
+            "link",
+        ]
+
+    class Meta:
+        template = 'streams/richtext_block.html'
+        icon = 'edit'
+        label = 'Simple Richtext'
