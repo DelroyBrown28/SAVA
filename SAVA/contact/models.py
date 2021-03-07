5376a01c06eed287843b9e074914d045c8fdf79d
Delroy Brown <delroybrown.db@gmail.com>
2021-03-07 12:26:44 +0000

    Form updated so it's placed on the page. no GSAP = sad times

--- SAVA/contact/models.py
+++ SAVA/contact/models.py
@@ -15,7 +15,6 @@ from wagtail.contrib.forms.models import (
 from streams import blocks
 
 
-
 class FormField(AbstractFormField):
     page = ParentalKey(
         'ContactPage',
@@ -28,8 +27,9 @@ class ContactPage(AbstractEmailForm):
     template = 'contact/contact_page.html'
 
     intro = RichTextField(blank=True)
+    thank_you_title = RichTextField(blank=True)
     thank_you_text = RichTextField(blank=True)
-    
+
     contact_page_panels = StreamField(
         [
             ('service_cards', blocks.CardBlock()),
@@ -41,7 +41,13 @@ class ContactPage(AbstractEmailForm):
     content_panels = AbstractEmailForm.content_panels + [
         FieldPanel('intro'),
         InlinePanel('form_fields', label='Form Fields'),
-        FieldPanel('thank_you_text'),
+        MultiFieldPanel([
+
+            FieldPanel('thank_you_title'),
+            FieldPanel('thank_you_text'),
+
+
+        ], heading='Thank You Page Text'),
         MultiFieldPanel([
             FieldRowPanel([
                 FieldPanel('from_address', classname='col6'),
